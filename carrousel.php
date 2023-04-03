5de7012c2a3b5090e375a22bafba5357fd1142d3
Noémie dS <112022872+NoemieDS@users.noreply.github.com>
2023-04-03 13:05:08 -0400

    S10C1 Dépôt initial du carrousel

---
+++ carrousel.php
@@ -0,0 +1,9 @@
+<?php
+  /*
+  * Plugin name: Carrousel Noémie da Silva
+  * Description: cette extension carrousel permettra d'afficher dans une boîte modal animé les images d'une galerie
+  * Version: 1.0
+  * Author: Noémie da Silva, d'après Eddy Martin
+  * Author URI: 
+  */
+?>
