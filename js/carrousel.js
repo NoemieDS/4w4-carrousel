472f80d4b17c004aed62c6a79309ead905793788
Noémie dS <112022872+NoemieDS@users.noreply.github.com>
2023-04-14 10:34:56 -0400

    S11C1 lab4 Carrousel de base fonctionnel (js)

--- js/carrousel.js
+++ js/carrousel.js
@@ -1,11 +1,41 @@
 (function() {
 console.log("Début du carrousel");
-}());
 
-let boutonOuvrir = document.getElementsByClassName("carrousel__ouvrir");
-let carrousel = document.getElementsByClassName("carrousel");
+let carrouselOuvrir = document.querySelector(".carrousel__ouvrir")
+let carrousel = document.querySelector(".carrousel")
+let carrouselX = document.querySelector(".carrousel__x")
+let galerie = document.querySelector(".galerie")
+let galerieImg = galerie.querySelectorAll("img")
+let carrouselFigure = document.querySelector(".carrousel__figure")
 
-console.log("Le bouton", boutonOuvrir);
+console.log("Le bouton", carrouselOuvrir);
 console.log("Le carrousel", carrousel);
 
+/* ----------------------------------------------------  ouvrir la boîte modale */
+carrouselOuvrir.addEventListener('mousedown', function(){
+console.log('ouvrir la boîte')
+ carrousel.classList.add('carrousel--activer')
+ AjouterLesImagesDeLaGalerie()
+})
 
+/* ----------------------------------------------------  fermer la boîte modale */
+carrouselX.addEventListener('mousedown', function(){
+  console.log('fermer la boîte')
+carrousel.classList.remove('carrousel--activer')
+ })
+
+ /* -------------------------------------------------------  ajouter des images */
+function AjouterLesImagesDeLaGalerie() {
+
+  for (const elt of galerieImg) {
+    let img = document.createElement("img")
+    img.classList.add("carrousel__img")
+    img.src =elt.src
+    console.log(img.src)
+    carrouselFigure.appendChild(img)
+  }
+
+}
+
+
+})()
